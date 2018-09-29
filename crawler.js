76fb7ece0fef8010271ca3a7e2723e2e280fb037
Ashley <ashley.ratigan@gmail.com>
2018-09-28 21:41:03 -0400

    Formated files to console.log success, skipped, error

--- crawler.js
+++ crawler.js
@@ -27,51 +27,82 @@ const validUrls = function (data) {
     }
     return urls
 }
-// console.log (validUrls(internetDataOne))
-let urls = validUrls(internetDataOne)
-// console.log(urls)
 
-for (let i = 0; i < internetDataOne.length; i++) {
-    // console.log(internetDataOne[i].address)
-    // console.log(urls)
-    success.push(internetDataOne[i].address)
-    // for (let i = 0; i < internetDataOne[i].links.length; i++) {
-        // console.log(internetDataOne[i])
-        let links = internetDataOne[i].links
+const crawler = function (data) {
+    let urls = validUrls(data)
+
+    for (let i = 0; i < data.length; i++) {
+        success.push(data[i].address)
+        let links = data[i].links
         for (let j = 0; j < links.length; j++) {
             // console.log(links)
             if (success.includes(links[j])) {
                 skipped.push(links[j])
                 // console.log(links[j])
             } 
-            if (!urls.includes(links[j])){
+            if (!urls.includes(links[j]) ){  // && !errror.includes(links[j]) -> duplicate error for page 1
                 error.push(links[j])
                 // console.log(error)
             }
         }
-        if (!urls.includes(internetDataOne[i].address)){
-            error.push(internetDataOne[i].address)
-            console.log(error)
-        }
-        // for (let k = 0; k < links.length; k++) {
-        //     console.log(success.includes(links[k]))
-        //     if (!success.includes(links[k])) {
-        //         error.push(links[k])
-        //         // console.log(links[j])
-        //     } 
-        //     console.log(error)
-        // }
-
-        // internetDataOne[i].links.forEach(function(link){
-        //     links.push(link)
-        // })
-        // console.log(links)
-        // if (success.includes()) {
-        //     skipped.push(internetDataOne[i].links[i])
-        // }
     }
-// }
+    console.log(`Success: ${success}`)
+    console.log(`Skipped: ${skipped}`)
+    console.log(`Error: ${error}`)
+    return data
+    // return[success, skipped, error]
+    // return Math.PI
+}
 
-console.log(success)
-console.log(skipped)
-console.log(error)
+// validUrls(internetDataOne)
+// crawler(internetDataOne)
+crawler(internetDataTwo)
+
+// // console.log (validUrls(internetDataOne))
+// // let urls = validUrls(internetDataOne)
+// // console.log(urls)
+
+// for (let i = 0; i < internetDataOne.length; i++) {
+//     // console.log(internetDataOne[i].address)
+//     // console.log(urls)
+//     success.push(internetDataOne[i].address)
+//     // for (let i = 0; i < internetDataOne[i].links.length; i++) {
+//         // console.log(internetDataOne[i])
+//         let links = internetDataOne[i].links
+//         for (let j = 0; j < links.length; j++) {
+//             // console.log(links)
+//             if (success.includes(links[j])) {
+//                 skipped.push(links[j])
+//                 // console.log(links[j])
+//             } 
+//             if (!urls.includes(links[j])){
+//                 error.push(links[j])
+//                 // console.log(error)
+//             }
+//         }
+//         if (!urls.includes(internetDataOne[i].address)){
+//             error.push(internetDataOne[i].address)
+//             console.log(error)
+//         }
+//         // for (let k = 0; k < links.length; k++) {
+//         //     console.log(success.includes(links[k]))
+//         //     if (!success.includes(links[k])) {
+//         //         error.push(links[k])
+//         //         // console.log(links[j])
+//         //     } 
+//         //     console.log(error)
+//         // }
+
+//         // internetDataOne[i].links.forEach(function(link){
+//         //     links.push(link)
+//         // })
+//         // console.log(links)
+//         // if (success.includes()) {
+//         //     skipped.push(internetDataOne[i].links[i])
+//         // }
+//     }
+// // }
+
+// console.log(success)
+// console.log(skipped)
+// console.log(error)
