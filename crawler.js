0672eb266b145baa91c16ed019400c53b14d1a77
Ashley <ashley.ratigan@gmail.com>
2018-09-28 21:18:54 -0400

    Now get invalid urls as well

--- crawler.js
+++ crawler.js
@@ -20,9 +20,20 @@ let invalid = []
 // returns first link in array
 // console.log(internetDataOne[0].links.length)
 
-
+const validUrls = function (data) {
+    let urls = []
+    for (let i = 0; i < data.length; i++) {
+        urls.push(data[i].address)
+    }
+    return urls
+}
+// console.log (validUrls(internetDataOne))
+let urls = validUrls(internetDataOne)
+// console.log(urls)
 
 for (let i = 0; i < internetDataOne.length; i++) {
+    // console.log(internetDataOne[i].address)
+    // console.log(urls)
     pages.push(internetDataOne[i].address)
     // for (let i = 0; i < internetDataOne[i].links.length; i++) {
         // console.log(internetDataOne[i])
@@ -32,8 +43,24 @@ for (let i = 0; i < internetDataOne.length; i++) {
             if (pages.includes(links[j])) {
                 duplicates.push(links[j])
                 // console.log(links[j])
+            } 
+            if (!urls.includes(links[j])){
+                invalid.push(links[j])
+                // console.log(invalid)
             }
         }
+        if (!urls.includes(internetDataOne[i].address)){
+            invalid.push(internetDataOne[i].address)
+            console.log(invalid)
+        }
+        // for (let k = 0; k < links.length; k++) {
+        //     console.log(pages.includes(links[k]))
+        //     if (!pages.includes(links[k])) {
+        //         invalid.push(links[k])
+        //         // console.log(links[j])
+        //     } 
+        //     console.log(invalid)
+        // }
 
         // internetDataOne[i].links.forEach(function(link){
         //     links.push(link)
@@ -42,8 +69,9 @@ for (let i = 0; i < internetDataOne.length; i++) {
         // if (pages.includes()) {
         //     duplicates.push(internetDataOne[i].links[i])
         // }
-    // }
-}
+    }
+// }
 
 console.log(pages)
 console.log(duplicates)
+console.log(invalid)
